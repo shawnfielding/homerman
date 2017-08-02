9b85c6f958739d09976b3b4a4e47c24c0bb22ee7
Shawn Fielding <shawnfielding@Shawns-MacBook-Pro.local>
2017-08-02 10:08:55 -0400

    getting cheerio to work

--- server.js
+++ server.js
@@ -1,32 +1,49 @@
 // Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var cheerio = require('cheerio');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cheerio = require('cheerio')
+const $ = cheerio.load('<h2 class="title">Hello world</h2>')
+const app = express();
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+}));
 app.use(express.static(`${__dirname}/public/`));
 
-// Create our server
+function getWord(theNum) {
+  console.log("made it to getword");
+  $.get("http://www.wordbyletter.com/words_starting_with.php?q=a&letters=11", function(err, res, body) {
+    if (err) {
+      return console.error(err)
+    };
+    $ = cheerio.load(body);
+    console.log($('#largemenu').next());
+    console.log("made it through getword");
+  });
+};
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
 app.post('/', function(req, res) {
-  var num = req.body
-  //console.log(req.body.);
-  console.log(num.numberOfLetters);
-  //console.log(num);
-  res.json(num.numberOfLetters);
+  var num = parseInt(req.body.numberOfLetters);
+  getWord(num);
+  res.json(num);
 });
+
 // Starts our server
 app.listen(PORT, function() {
   console.log('Server is listening on PORT: ' + PORT);
+
+
 });
